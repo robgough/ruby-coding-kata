9682d2fd2f6e9e0b1b9eaf1a10c97f465108a3b0
Rob Gough <robert.gough@onthebeach.co.uk>
2013-10-10 17:50:39 +0100

    refactor

--- bowling_rspec.rb
+++ bowling_rspec.rb
@@ -28,8 +28,8 @@ describe 'bowling kata' do
     expect(calculate_score(bowls)).to eq(39)
   end
   it 'should be able to correctly score a turkey' do
-    bowls = [[10], [10], [10]]
-    expect(calculate_score(bowls)).to eq(60)
+    bowls = [[10], [10], [10], [1,0]]
+    expect(calculate_score(bowls)).to eq(63)
   end
   it 'should be able to handle an endgame with a spare' do
     bowls = [[7, 3, 2]]
@@ -59,35 +59,33 @@ describe 'bowling kata' do
     bowls = [[5,3],[5,2],[2,6],[9,1],[3,7],[10],[6,2],[5,2],[5,4],[2,8,10]]
     expect(calculate_score(bowls)).to eq(118)
   end
+  it 'should be able to correctly score a full game' do
+    bowls = [[10],[10],[10],[10],[10],[10],[10],[10],[10],[10,10,10]]
+    expect(calculate_score(bowls)).to eq(300)
+  end
 end
 
 def calculate_score(bowls)
   score = 0
-  frames = []
-  to_be_added = []
+  frames = bowls.inject([]) { |a, f| a << Frame.new(f) }
 
-  bowls.each do |f|
-    frames << Frame.new(f)
-  end
-  
   frames.each_with_index do |frame, i|
-    next_throw, next_again_throw = get_next_throws(frames, i)
-    score += frame.score(next_throw, next_again_throw)
+    score += frame.score(*get_next_throws(frames, i))
   end
   score
 end
 
 def get_next_throws(frames, i)
   next_throw, next_again_throw = 0,0
-  return 0,0 if frames[i+1].nil?
+  return 0,0 unless frames[i+1]
   next_throw = frames[i+1].bowls[0]
   # if a strike was scored, then we need to jump ahead *again*
   if next_throw == 10
-    next_again_throw = frames[i+2].bowls[0] if !frames[i+2].nil?
+    next_again_throw = frames[i+2].bowls[0] if frames[i+2]
   else
     next_again_throw = frames[i+1].bowls[1]
   end 
-  return next_throw, next_again_throw
+  [next_throw, next_again_throw]
 end
 
 class Frame
@@ -95,13 +93,23 @@ class Frame
 
   def initialize(bowls)
     @bowls = bowls
-    if @bowls.length < 2
-      @bowls << 0
-    end
   end
 
-  def last?
-    @bowls.count > 2
+  def score(next_throw, next_again_throw)
+    total_score = sum_bowls
+
+    return total_score unless next_throw 
+
+    total_score += next_throw if spare? || strike?
+    total_score += next_again_throw if strike?
+
+    total_score
+  end
+
+  private
+
+  def sum_bowls
+    @bowls.inject :+
   end
 
   def strike?
@@ -109,32 +117,6 @@ class Frame
   end
 
   def spare?
-    !last? && @bowls.first + @bowls.last == 10
-  end
-
-  def score(next_throw, next_again_throw)
-    total_score = @bowls[0] + @bowls[1]
-    
-    return total_score if next_throw.nil?
-    return score_endgame if @bowls.count > 2
-
-    total_score += next_throw if spare? || strike?
-    total_score += next_again_throw if !next_throw.nil? && strike?
-
-    total_score
-  end
-
-  private
-
-  def score_endgame
-    score = 0
-    if @bowls.length < 3
-      score += @bowls[0] + @bowls[1]
-    else
-      score += @bowls.inject(0) do |sum, bowl|
-                sum += bowl
-               end
-    end
-    score
+    sum_bowls == 10
   end
 end
--- harry_potter_rspec.rb
+++ harry_potter_rspec.rb
@@ -1,5 +1,36 @@
 describe 'harry potter kata' do
+  let(:pos) { PointOfSale.new } 
 
-  
+  it 'should cost 8 EUR for one book' do
+    price_in_eur = pos.calculate_price [1]
+    expect(price_in_eur).to eq(8)
+  end
 
+  it 'should have 5% discount on two different books' do
+    price = pos.calculate_price [1, 2]
+    expect(price).to eq(60.80) # 5% = 3.20
+  end
+
+  it 'should have no discount on two of the same books' do
+    price = pos.calculate_price [1, 1]
+    expect(price).to eq(16)
+  end
+end
+
+class PointOfSale
+  def calculate_price(books)
+    price = 0
+    
+    if books.length == 1
+      price = 8
+    end
+
+    if books.length == 2
+      if books[0] == books [1]
+        price = 
+        price = 16
+    end
+
+    price
+  end
 end
